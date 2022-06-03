92173bf204b8d9baf99f935768ed333d2093f644
NosSLV <90413107+NosSLV@users.noreply.github.com>
2022-06-03 17:21:44 +0200

    Update setup.sh

--- pipelines/PIPELINE-FULL-STAGING/setup.sh
+++ pipelines/PIPELINE-FULL-STAGING/setup.sh
@@ -10,14 +10,14 @@ python3.7 -m venv todo-list-aws
 source todo-list-aws/bin/activate
 # Upgrade Pip
 python -m pip install --upgrade pip
-#For static testing
+# For static testing
 python -m pip install radon
 python -m pip install flake8
 python -m pip install flake8-polyfill
 python -m pip install bandit
-#For integration testing
+# For integration testing
 python -m pip install pytest
-#For unit testing
+# For unit testing
 python -m pip install boto3
 python -m pip install moto
 python -m pip install mock==4.0.2
